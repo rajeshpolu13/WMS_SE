2eaa4125d3e73c84f5dc3192cb006ab43740fe4f
rajeshpolu13 <npolu@albany.edu>
2023-11-26 22:53:03 -0500

    fix router issue

--- wmsui/src/App.js
+++ wmsui/src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
 import Navigationbar from './navbar/Navbar';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter,BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import store from './redux-part/store';
 function App() {
   return (
     <div className="App">
+      
       <Provider store={store}>
-        <BrowserRouter>
+      <HashRouter>
+     
             <Navigationbar />
-      </BrowserRouter>
+   
+      </HashRouter>
       </Provider>
+     
     </div>
   );
 }
